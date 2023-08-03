4676c75a3ce15742a798fdcf2ca49932e7c48aeb
kyohei-p <ysdkyh11@gmail.com>
2023-08-03 22:42:03 +0900

    usersテーブルへカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_03_132037) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_03_133405) do
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_03_132037) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "self_introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
