95741a0ece86133e14a04215c2a6afbbc6f9da91
kyohei-p <ysdkyh11@gmail.com>
2023-08-19 16:03:26 +0900

    avaterカラムのデータ型変更

--- db/schema.rb
+++ db/schema.rb
@@ -68,7 +68,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_18_205313) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "self_introduction"
-    t.string "avater"
+    t.binary "avater"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
