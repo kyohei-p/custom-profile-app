3651701b14c6e21c311c48221bc86dcb0c39e1e7
kyohei-p <ysdkyh11@gmail.com>
2023-08-27 20:53:56 +0900

    ログインページのバリデーション作成

--- README.md
+++ README.md
@@ -54,6 +54,7 @@ Ruby on Rails | 7.0.4.2
 - [pry-rails](https://github.com/pry/pry-rails)
 - [rspec-rails](https://github.com/rspec/rspec-rails)
 - [factory_bot_rails](https://github.com/thoughtbot/factory_bot_rails)
+- [devise](https://github.com/heartcombo/devise)
 
 ### Rspec初期化〜テスト実行
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable
+         :recoverable, :rememberable, :validatable
 
   has_many :skills
   has_one_attached :image
 
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
   validates :password, presence: true, if: -> { password.present? }
   validates :self_introduction, presence: true, length: { minimum: 50, maximum: 200 }
 end
