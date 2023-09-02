b6f0dafeff110f86d520f28d0e36527442e12104
kyohei-p <ysdkyh11@gmail.com>
2023-09-02 09:55:35 +0900

    データ100未満の時、y軸最大値100

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,9 +13,9 @@ class UsersController < ApplicationController
         @categories.each do |category|
           skills = Skill.where(category_id: category.id)
           monthly_skill_levels = {
-            "先々月" => skills.where(updated_at: 3.months.ago.beginning_of_month..2.months.ago.end_of_month).sum(:skill_level),
-            "先月" => skills.where(updated_at: 2.months.ago.beginning_of_month..1.month.ago.end_of_month).sum(:skill_level),
-            "今月" => skills.where(updated_at: 1.month.ago.beginning_of_month..Time.current.end_of_month).sum(:skill_level)
+            "先々月" => skills.where(updated_at: 2.months.ago.beginning_of_month).sum(:skill_level),
+            "先月" => skills.where(updated_at: 1.months.ago.beginning_of_month).sum(:skill_level),
+            "今月" => skills.where(updated_at: Time.current.beginning_of_month..Time.current.end_of_month).sum(:skill_level)
           }
 
           @data[category.name] = monthly_skill_levels
